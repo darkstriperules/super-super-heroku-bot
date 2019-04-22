7892834639debefcc118371510022fb03c4014db
darkstriperules <49772803+darkstriperules@users.noreply.github.com>
2019-04-21 18:49:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!cat') {
+    	message.reply('meow');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTYzMTgyMTIzNjY2ODk4OTU0.XL0dWg.7GN-I9WeN_NbTL2fYQ8vVW7pVF8);
